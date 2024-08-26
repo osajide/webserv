231cd2bb1dcaed1c73752bc7c3df92360046af47
OussamaLatitude <osajide5@gmail.com>
2024-08-26 18:53:11 +0100

    add sys/wait.h header file to cgi.cpp file

--- srcs/cgi.cpp
+++ srcs/cgi.cpp
@@ -8,7 +8,7 @@
 #include <cstdio>
 #include <dirent.h>
 #include "../inc/error.hpp"
-// #include <wait.h>
+#include <sys/wait.h>
 
 cgi::cgi() : _pid(-1), _exit_status(-1), _cgi_processing(false), _env(NULL), _args(NULL), _first_time(true), _outfile(""),
 				_infile("")
