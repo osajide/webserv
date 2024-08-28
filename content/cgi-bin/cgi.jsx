38a774ebe61c66661be2e147e90f8d9c10b6370c
Yak <yassinekhayri123@gmail.com>
2024-08-28 15:50:24 +0100

    Node post done

--- content/cgi-bin/cgi.jsx
+++ content/cgi-bin/cgi.jsx
@@ -3,6 +3,35 @@
 meth = process.env.REQUEST_METHOD
 data = ''
 
+const displayRes = () => {
+	data = decodeURIComponent(data?.replace('+', ' ')).split('&')
+	
+	body = `
+		<!DOCTYPE html>
+		<html lang="en">
+	
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>CGI</title>
+			<link rel="stylesheet" href="/cgi-bin/forms/styles/style.css" />
+		</head>
+	
+		<body>
+			<div class="response">
+				<h1>Methode: ${meth}</h1>
+				<h2>${data[2]?.replace('=', ': ')}</h2>
+				<h2>${data[3]?.replace('=', ': ')}</h2>
+				<h2>${data[4]?.replace('=', ': ')}</h2>
+			</div>
+		</body>
+	
+		</html>
+	`
+	
+	console.log(`HTTP/1.1 200 OK\r\nContent-Length: ${body.length}\r\nContent-Type: text/html\r\n\r\n${body}`)
+}
+
 if (meth == 'POST') { 
 	const readline = require('readline');
 
@@ -12,38 +41,15 @@ if (meth == 'POST') {
 	});
 
 	rl.on('line', (line) => {
-		console.error(line)
 		if (line)
 			data += line
 	});
+
+	rl.on('close', () => {
+		displayRes()
+	});
 }
-else
+else {
 	data = process.env.QUERY_STRING
-
-data = decodeURIComponent(data?.replace('+', ' ')).split('&')
-
-body = `
-	<!DOCTYPE html>
-	<html lang="en">
-
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>CGI</title>
-		<link rel="stylesheet" href="/cgi-bin/forms/styles/style.css" />
-	</head>
-
-	<body>
-		<div class="response">
-			<h1>Methode: ${meth}</h1>
-			<h2>${data[2]?.replace('=', ': ')}</h2>
-			<h2>${data[3]?.replace('=', ': ')}</h2>
-			<h2>${data[4]?.replace('=', ': ')}</h2>
-			<h2>${JSON.stringify(data)}</h2>
-		</div>
-	</body>
-
-	</html>
-`
-
-console.log(`HTTP/1.1 200 OK\r\nContent-Length: ${body.length}\r\nContent-Type: text/html\r\n\r\n${body}`)
+	displayRes()
+}
